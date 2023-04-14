495233810ea2bb0c6deaebed75f2139fceb5ff66
FredrickUdom <fredrickudom@gmai.com>
2023-04-14 15:12:21 +0100

    this is an empty nestjs project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('ub')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHelloWorld();
   }
 }
